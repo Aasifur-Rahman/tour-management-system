0f328b59ba1d4bd75ba5ea48ed6f7161962312de
Siam Rahman <asifur.web@gmail.com>
2025-07-12 21:16:55 +0600

    setup completed

---
+++ src/server.ts
@@ -0,0 +1,76 @@
+import app from "./app";
+import mongoose from "mongoose";
+import { Server } from "http";
+import { envVars } from "./app/config/env";
+// make this first
+let server: Server;
+
+// 2nd one make this function with try catch first then go inside
+const startServer = async () => {
+  try {
+    // connect to mongodbatlas via connection string
+    console.log(envVars.NODE_ENV);
+    await mongoose.connect(envVars.DB_URL);
+    console.log("Connected to DB");
+
+    // 3rd make this listening to the port like this
+    server = app.listen(5000, () => {
+      console.log(`Server is listening to port ${envVars.PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
+
+// unhandled rejection error
+process.on("unhandledRejection", (err) => {
+  console.log("Unhandled Rejection detected... Server shutting down...", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+// uncaught exception
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught Exception detected... Server shutting down...", err);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+// Signal termination
+process.on("SIGTERM", () => {
+  console.log("SIGTERM signal recieved... Server shutting down...");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+// sigint for manually check for that so this is why we use it here and sigterm for cloud managers
+process.on("SIGINT", () => {
+  console.log("SIGINT signal recieved... Server shutting down...");
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  }
+
+  process.exit(1);
+});
+
+// Promise.reject(new Error("I forgot to catch this promise"));
+// throw new Error("I forgot to handle this local error");
