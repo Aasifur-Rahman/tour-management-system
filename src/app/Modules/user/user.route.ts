f561366e277a873736799ec382cb1943fc89a7ba
Siam Rahman <asifur.web@gmail.com>
2025-08-20 21:05:26 +0600

    added router patch

--- src/app/Modules/user/user.route.ts
+++ src/app/Modules/user/user.route.ts
@@ -17,5 +17,13 @@ router.get(
   checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
   UserControllers.getAllUsers
 );
+// /api/v1/user/:id
+// what is happening here with the checkAuth having spread object values ?
+// => basically this object.value it will have the role which is value of array and by spreading we are getting what we needed
+router.patch(
+  "/:id",
+  checkAuth(...Object.values(Role)),
+  UserControllers.updateUser
+);
 
 export const UserRoutes = router;
